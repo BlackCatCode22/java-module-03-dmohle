ebfeaef3503358b8fccd21675894775e8c1b0939
dmohle <10038913+dmohle@users.noreply.github.com>
2024-09-17 12:19:56 -0700

    Add files via upload

--- Dog.java
+++ Dog.java
@@ -1,13 +1,57 @@
-package myanimals;
+public class Dog {
+    // Create two fields for our Dog objects.
 
-public class Dog extends Animal{
-    // the Dog class is a subclass of Animal.
-    // so it should have all the methods from the Animal class
-    // available to it.
-    // This is polymorphism - this word means "many shapes"
-    // when used here, by a Dog object, this method that has the same name
-    // as the method in the superclass, take another shape
-    public void animalSound() {
-        System.out.println("\n A sound from the Dog class used by a Dog Object - 'bark'");
+    private String name;
+    private int age;
+
+    // Create getters and setters for our fields.
+
+    // Create getter and setter for name.
+    public void setName(String name) {
+        this.name = name;
     }
+    public String getName() {
+        return name;
+    }
+
+    // Create getter and setter for age.
+    public int getAge() {
+        return this.age;
+        // this would work as well because there is no ambiguity
+        // return age;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+
+
+
+
+
+
+    // Create a constructor for the Dog objects that will be created with the "new" keyword.
+    // A constructor is a special method that is called or invoked when... the Dog class is used
+    // to create an object.
+    public Dog() {
+        System.out.println("\n A dog object was created!! This message is from the constructor. \n");
+    }
+
+    // Create a constructor that accepts a String data type for the Dog's name.
+    public Dog(String myName) {
+        this.name = myName;
+    }
+
+    // Create a constructor that accepts an argument when an object is created, this argument will
+    // be an int
+    public Dog(int theAge){
+        this.age = theAge;
+    }
+
+    // Create a constructor that accepts two arguments - a String for the name, and an int for age.
+     public Dog(String someName, int someAge){
+        // Assign name and age to our new object.
+        this.age = someAge;
+        this.name = someName;
+    }
+
 }
